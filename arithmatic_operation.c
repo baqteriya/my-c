a7a66cc4a227869bbf30307bf890c5011bda95b6
baqteriya <kumaririya1030@gmail.com>
2024-05-06 11:34:36 +0530

    Create arithmatic_operation.c

---
+++ arithmatic_operation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=100,b=50;
+    int sum,subtract,multiply,division;
+    sum=a+b;
+    subtract=a-b;
+    multiply=a*b;
+    division=a/b;
+    printf("sum of a and b=%d",sum);
+    printf("\n subtract of a and b=%d",subtract);
+    printf("\n multiply of a and b=%d",multiply);
+    printf("\n division of a and b=%d",division);
+    return(0);
+}
+
