a7b04483f69d18fe8aceb82fe8e7384517dd3221
baqteriya <kumaririya1030@gmail.com>
2024-05-09 19:45:15 +0530

    Create even_number.c

---
+++ even_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int num ;
+    printf("enter the number");
+    scanf("%d",& num);
+    if(num%2==0)
+printf("\n given number is even number");
+return(0);
+}
+
